109f81f7d0d597a3249e06e3e0fd4009292b2134
MissVeronica <veronica@tranza.se>
2022-10-25 15:29:17 +0200

    Update um-count-users.php

--- um-count-users.php
+++ um-count-users.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name:     Ultimate Member - Count Users
  * Description:     Extension to Ultimate Member with a shortcode [um_count_users_custom] to list number of users with same meta_values for a meta_key.
- * Version:         1.0.0
+ * Version:         2.0.0
  * Requires PHP:    7.4
  * Author:          Miss Veronica
  * License:         GPL v2 or later
@@ -18,9 +18,13 @@ if( !class_exists( 'UM' )) return;
 add_shortcode( 'um_count_users_custom', 'um_count_users_custom_shortcode' );
 
 function um_count_users_custom_shortcode( $atts, $contents = null ) {
-    global $wpdb;
+	global $wpdb;
     if( empty( $atts['meta_key'] )) return 'No meta_key';
     $meta_key = sanitize_text_field( $atts['meta_key'] );
+    if( isset( $atts['countryflags'] ) && $atts['countryflags'] == 'true' ) {
+        $country_flags = true;
+        $countries = UM()->builtin()->get( 'countries' ); 
+    } else $country_flags = false;
     $users = $wpdb->get_col( "
                     SELECT meta_value
                     FROM {$wpdb->usermeta}
@@ -41,6 +45,14 @@ function um_count_users_custom_shortcode( $atts, $contents = null ) {
                     }
                 } else $empty++;
             } else {
+                if( $country_flags ) {
+                    $country_code = array_search( $value, $countries );
+                    if( empty( $country_code )) {
+                        if( array_key_exists( $value, $countries )) {
+                            $value = $countries[$value];
+                        } else $value = 'unknown ' . $value;
+                    }
+                }
                 if( isset( $options[$value] )) $options[$value]++;
                 else $options[$value] = 1;
             }
@@ -59,11 +71,16 @@ function um_count_users_custom_shortcode( $atts, $contents = null ) {
     }    
     foreach( $options as $key => $count ) {
         echo '<div style="display: table-row;">';
+        if( defined( 'um_country_flag_url' ) && $country_flags ) {
+            $country_code = array_search( $key, $countries );
+?>
+            <span class="flag-icon flag-icon-<?php echo strtolower( $country_code );?>" aria-label="<?php echo $key;?>"></span>
+            <span style="padding-right: 10px"></span>
+<?php
+        }
         echo '<div style="display: table-cell; padding-right: 10px">' . $key . '</div>';
         echo '<div style="display: table-cell; text-align: right;">' . $count . '</div>';
         echo '</div>';
     }
-    $output = ob_get_contents();
-    ob_end_clean();
-    return $output;
+    return ob_get_clean();
 }
